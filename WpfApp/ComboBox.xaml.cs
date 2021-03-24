7517446e13b5e28c07ae05fed2f2940c55055514
bill1411 <346278334@qq.com>
2021-03-24 10:26:51 +0800

    初始化

---
+++ WpfApp/ComboBox.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// ComboBox.xaml 的交互逻辑
+    /// </summary>
+    public partial class ComboBox : Window
+    {
+        public ComboBox()
+        {
+            InitializeComponent();
+        }
+
+        #region  通过字典形式赋值，取值(设置SelectedValuePath和DisplayMemberPath)
+        /// <summary>
+        /// 通过字典形式赋值，取值(设置SelectedValuePath和DisplayMemberPath)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ComboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            Dictionary<int, string> mydic = new Dictionary<int, string>()
+            {
+                {1,"a"},{2,"b"},{3,"c"}
+            };
+            //设置数据源
+            combobox.ItemsSource = mydic;
+            //设置实际的值
+            combobox.SelectedValuePath = "Key";
+            //设置显示的值
+            combobox.DisplayMemberPath = "Value";
+        }
+
+        /// <summary>
+        /// 获取选定的值
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            KeyValuePair<int, string> dict = (KeyValuePair<int, string>)combobox.SelectedItem;
+
+            MessageBox.Show(dict.Key.ToString());
+            MessageBox.Show(dict.Value.ToString());
+        }
+
+        #endregion
+
+        #region 通过List<string>赋值，取值(默认不设置SelectedValuePath和DisplayMemberPath)
+        private void Combobox2_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> list = new List<string>();
+            list.Add("中国");
+            list.Add("俄罗斯");
+            list.Add("英国");
+            list.Add("法国");
+            combobox2.ItemsSource = list;
+
+        }
+        private void Combobox2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string ss = combobox2.SelectedItem.ToString();
+            MessageBox.Show(ss);
+        }
+        #endregion
+
+        #region 通过List<string>赋值，取值(默认设置SelectedValuePath属性)
+        private void Combobox3_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string ss = combobox3.SelectedItem.ToString();
+            MessageBox.Show(ss);
+        }
+
+        private void Combobox3_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            List<string> list = new List<string>();
+            list.Add("美国");
+            list.Add("日本");
+            list.Add("德国");
+            list.Add("加拿大");
+            combobox3.ItemsSource = list;
+            combobox3.SelectedValuePath = "Key";
+        }
+        #endregion
+    }
+}
---
+++ WpfApp/DataGrid.xaml.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// DataGrid.xaml 的交互逻辑
+    /// </summary>
+    public partial class DataGrid : Window
+    {
+        List<people> peopleList = new List<people>();
+
+        #region 实体对象定义
+        public enum sexual_enum
+        {
+            BOY,
+            GIRL
+        }
+        public class people
+        {
+            public int id { get; set; }
+            public string name{ get; set; }
+            public string age { get; set; }
+            public sexual_enum sexual { get; set; }
+        }
+        #endregion
+        public DataGrid()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataInit();
+            dataGrid.ItemsSource = peopleList;
+        }
+
+        #region 获取当前选中项的ID值
+        private List<int> GetSelectedIds()
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < this.dataGrid.Items.Count; i++)
+            {
+                var cntr = dataGrid.ItemContainerGenerator.ContainerFromIndex(i);
+                DataGridRow ObjROw = (DataGridRow)cntr;
+                if (ObjROw != null)
+                {
+                    FrameworkElement objElement = dataGrid.Columns[0].GetCellContent(ObjROw);
+                    if (objElement != null)
+                    {
+                        System.Windows.Controls.CheckBox objChk = (System.Windows.Controls.CheckBox)objElement;
+                        if (objChk.IsChecked == true)
+                        {
+                            list.Add(i);   //选中的加到集合中
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+        #endregion
+
+        #region 初始化数据
+        private void DataInit()
+        {
+            peopleList.Add(new people()
+            {
+                id = 458,
+                name = "小明",
+                age = "18",
+                sexual = sexual_enum.BOY,
+            });
+            peopleList.Add(new people()
+            {
+                id = 489,
+                name = "小红",
+                age = "20",
+                sexual = sexual_enum.GIRL
+            });
+        }
+        #endregion
+
+        #region  全选
+        /// <summary>
+        /// 全选
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ckbSelectedAll_Checked(object sender, RoutedEventArgs e)
+        {
+            //this.dataGrid.SelectAll();
+            for (int i = 0; i < this.dataGrid.Items.Count; i++)
+            {
+                var cntr = dataGrid.ItemContainerGenerator.ContainerFromIndex(i);
+                DataGridRow ObjROw = (DataGridRow)cntr;
+                if (ObjROw != null)
+                {
+                    FrameworkElement objElement = dataGrid.Columns[0].GetCellContent(ObjROw);
+                    if (objElement != null)
+                    {
+                        System.Windows.Controls.CheckBox objChk = (System.Windows.Controls.CheckBox)objElement;
+                        if (objChk.IsChecked == false)
+                        {
+                            objChk.IsChecked = true;
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region  全不选
+        /// <summary>
+        /// 全不选
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ckbSelectedAll_Unchecked(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < this.dataGrid.Items.Count; i++)
+            {
+                var cntr = dataGrid.ItemContainerGenerator.ContainerFromIndex(i);
+                DataGridRow ObjROw = (DataGridRow)cntr;
+                if (ObjROw != null)
+                {
+                    FrameworkElement objElement = dataGrid.Columns[0].GetCellContent(ObjROw);
+                    if (objElement != null)
+                    {
+
+                        System.Windows.Controls.CheckBox objChk = (System.Windows.Controls.CheckBox)objElement;
+                        if (objChk.IsChecked == false)
+                        {
+                            objChk.IsChecked = true;
+                        }
+                        else
+                        {
+                            objChk.IsChecked = false;
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<int> list = GetSelectedIds();
+            foreach (var item in list)
+            {
+                MessageBox.Show("选中行当Index是：" + item);
+            }
+            
+        }
+    }
+}
